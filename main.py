ab828e4469f96d50e24b1353dd4f705f5830e8c1
MartinPsyco <162153626+MartinPsyco@users.noreply.github.com>
2025-12-20 01:36:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ GROUP_ID = int(os.getenv("GROUP_ID"))
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 REPLACE_FROM = os.getenv("REPLACE_FROM")
 REPLACE_TO = os.getenv("REPLACE_TO")
+# Debug opcional: imprime credenciales para verificar que Railway las pasa bien
+print("API_ID:", API_ID)
+print("API_HASH:", API_HASH)
+print("BOT_TOKEN:", BOT_TOKEN[:10], "...")
 
 # Inicializa el cliente directamente con el bot token
 
@@ -24,4 +28,5 @@ print("✅ Telegram Reposter online 🚀")
 
 # Mantiene el bot corriendo
 client.run_until_disconnected()
+
 
